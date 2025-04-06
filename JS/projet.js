6c42627dbe95f07d5a1aa7385406b1f0bf4f81b9
Justine <blin.justine.sio@gmail.com>
2025-04-07 01:36:42 +0200

    Modifications

--- JS/projet.js
+++ JS/projet.js
@@ -1,4 +1,4 @@
-document.querySelectorAll('.projet-card').forEach(card => {
+document.querySelectorAll('.card').forEach(card => {
     card.addEventListener('click', () => {
         const projetId = card.getAttribute('data-id');
         window.location.href = `projet-details.html?id=${projetId}`;

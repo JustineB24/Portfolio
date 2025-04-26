b6f0ab33d47987b3e46bf34817de56454c2be023
Justine <blin.justine.sio@gmail.com>
2025-04-26 02:05:34 +0200

    Modifications

--- JS/menuBurger.js
+++ JS/menuBurger.js
@@ -1,4 +1,10 @@
-function toggleMenu() {
-    var menu = document.querySelector('.nav-menu');
-    menu.classList.toggle('open'); // Ajoute/retire la classe 'open'
-}
+// Sélection des éléments
+const sideMenu = document.querySelector('.menu-burger');
+const iconMenu = document.querySelector('.icon-menu');
+const html = document.documentElement; // Utilisation de <html> pour appliquer le défilement
+
+// Gestion du clic sur l'icône menu (ouvrir/fermer le menu)
+iconMenu.addEventListener('click', function () {
+    sideMenu.classList.toggle('open'); // Basculer l'état du menu
+    html.style.overflow = sideMenu.classList.contains('open') ? 'hidden' : ''; // Gérer le défilement
+});
